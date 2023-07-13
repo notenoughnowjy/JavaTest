01b5b01061d05c6cecc170b0e1d1734196efab84
notenoughnowjy <jyp4188@gmail.com>
2023-07-13 09:46:37 +0900

    [2023.07.12] printstar

--- 2023.07.12/printstars2.java
+++ 2023.07.12/printstars2.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class printstars {
+public class printstars2 {
     public static void printStar(int n){
         for(int i=0; i<n; i++){
                 for(int j=0; j<n; j++){
