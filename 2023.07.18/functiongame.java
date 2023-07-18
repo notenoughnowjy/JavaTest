5b50738d8c26c72acba1dc420086723010678d95
notenoughnowjy <jyp4188@gmail.com>
2023-07-18 15:05:47 +0900

    [2023.07.18] / 값을 반환하는 함수 / 함수를 이용한 369 게임(while문 없데이트)

--- 2023.07.18/functiongame.java
+++ 2023.07.18/functiongame.java
@@ -1,12 +1,22 @@
+// https://www.codetree.ai/missions/5/problems/369-games-using-functions/description
+// 값을 반환하는 함수 / 함수를 이용한 369 게임
 import java.util.Scanner;
 
 public class functiongame{
   public static boolean contains369(int n){
-    return n % 10 == 3 || n % 10 == 6 || n % 10 == 9 || n / 10 == 3 || n / 10 == 6 || n / 10 == 9;
+    while(n>0){
+      if(n%10 == 3 || n % 10 == 6 || n % 10 == 9){
+        return true;
+      }
+      n /= 10;
+    }
+    return false;
   }
+
   public static boolean isthistrue(int n){
-    return contains369(n) || n % 3 == 0;
+    return contains369(n) || (n % 3 == 0);
   }
+
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
     int a = sc.nextInt();
@@ -15,6 +25,7 @@ public class functiongame{
     for(int i=a; i<=b; i++){
       if(isthistrue(i)){
         count++;
+        // System.out.println(i);
       }
     }
     System.out.println(count);
