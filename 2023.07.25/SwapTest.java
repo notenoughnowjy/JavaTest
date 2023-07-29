0e9dd2ef253f12776d55f4a7a2b2a8c4649352f3
notenoughnowjy <jyp4188@gmail.com>
2023-07-29 11:59:13 +0900

    [2023.07.29] / 변수의 영영 / 함수를 이용한 부분 문자열의 위치 구하기 / 다시 풀어봐야할 문제

---
+++ 2023.07.25/SwapTest.java
@@ -0,0 +1,5 @@
+public class SwapTest {
+  public static void main(String[] args) {
+    
+  }
+}
---
+++ 2023.07.28/function_string_position.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class function_string_position{
+  public static String input, output;
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+    input = sc.next();
+    output = sc.next();
+
+    if(isTrue())
+
+  }
+}
