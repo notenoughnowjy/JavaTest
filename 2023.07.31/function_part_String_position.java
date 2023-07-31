2ccd322623daf5b5cd7abdc5c75d487ed58a1a26
notenoughnowjy <jyp4188@gmail.com>
2023-07-31 11:36:57 +0900

    [2023.07.31] / 변수의 영역 / 함수를 이용한 부분 문자열의 위치 구하기 / [미해결]

---
+++ 2023.07.31/function_part_String_position.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class function_part_String_position{
+  public static String input;
+  public static String Part_String;
+  public static int result = -1;
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    input = sc.next();
+    Part_String = sc.next();
+
+    for(int i=0; i<input.length(); i++){
+      if(isTrue(i))
+        System.out.println(i);
+      else System.out.println(result);
+    }
+
+  }
+
+  private static boolean isTrue(int input_index) {
+    for(int i=0; i<Part_String.length(); i++){
+      if(input.charAt(input_index) == Part_String.charAt(i)){
+        return true;
+      }
+    }
+    return false;
+  }
+}
