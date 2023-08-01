73b02af3627a74cdf68b9850293c0e9a59b7c97e
notenoughnowjy <jyp4188@gmail.com>
2023-08-01 11:06:54 +0900

    [2023.08.01] / 재귀함수에서 print 위치의 중요성

---
+++ 2023.08.01/number_test.java
@@ -0,0 +1,20 @@
+
+
+public class number_test{
+  public static void main(String[] args) {
+
+    int n = 3;
+
+    printStar(n);
+  }
+  public static void printStar(int n) {
+    if(n==0)
+      return;
+    
+    printStar(n-1);
+    for(int i=0; i<n; i++){
+      System.out.print("*");
+    }
+    System.out.println();
+  }
+}
