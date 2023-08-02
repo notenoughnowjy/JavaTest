7caf3e68e1ccc554c37cafde9ac79660cc97ca0d
notenoughnowjy <jyp4188@gmail.com>
2023-08-02 09:43:27 +0900

    [2023.08.02] / 재귀함수를 이용한 별 출력

---
+++ 2023.08.02/recursionfunction_printStar.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class recursionfunction_printStar{
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+    printStar(n);
+  }
+
+  private static void printStar(int n) {
+    if(n == 0)
+      return;
+    printStar(n-1);
+    for(int i=0; i<n; i++){
+      System.out.print("*");
+    }
+    System.out.println();
+  }
+}
