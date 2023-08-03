1eaff6ac51d9ff5946dde5831ff557f4e68190e2
notenoughnowjy <jyp4188@gmail.com>
2023-08-03 10:28:53 +0900

    [2023.08.03] / 값을 반환하는 재귀함수 / 각 자리 숫자의 제곱

---
+++ 2023.08.03/recursion_part_sum.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class recursion_part_sum {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    System.out.println(recursion(n));
+  }
+
+  private static int recursion(int n) {
+    if(n<10)
+      return n * n;
+    return recursion(n/10) + ((n%10) * (n%10));
+  }
+}
