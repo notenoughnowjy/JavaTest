be4d922c01229e9eeb1973cd2603e6e4447fb7be
notenoughnowjy <jyp4188@gmail.com>
2023-08-07 11:30:18 +0900

    [2023.08.07] / 값을 반환하는 재귀함수 / 재귀함수를 이용한 최댓값 / Should Resolve

---
+++ SHOULD_RESOLVE/recursion_max_value.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class recursion_max_value{
+  public static int Max_N = 100;
+  public static int[] arr = new int[Max_N];
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    for(int i=0; i<n; i++)
+      arr[i] = sc.nextInt();
+      System.out.println(recursion_maxvalue(n-1));
+  }
+  
+  private static int recursion_maxvalue(int n){
+    if(n == 0)
+      return arr[0];
+    return Math.max(recursion_maxvalue(n-1), arr[n]);
+  }
+}
