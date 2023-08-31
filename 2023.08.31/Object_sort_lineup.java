161a2c5a3b56953c8fb7f03da451faf04dba7b4f
notenoughnowjy <jyp4188@gmail.com>
2023-08-31 12:17:19 +0900

    [2023.08.23] /객체 정렬 / 줄세우기

---
+++ 2023.08.31/Object_sort_lineup.java
@@ -0,0 +1,48 @@
+// https://www.codetree.ai/missions/5/problems/line-up-students/description
+// 객체 정렬 / 줄 세우기
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Student implements Comparable<Student>{
+  int number, height, weight;
+
+  public Student(int number, int height, int weight) {
+    this.number = number;
+    this.height = height;
+    this.weight = weight;
+  }
+  @Override
+  public int compareTo(Student student) {
+    // 키가 크면 정렬 했을 때 앞으로
+    if(this.height != student.height){
+      return student.height - this.height;
+    }
+    if(this.weight != student.weight){
+      return student.weight - this.weight;
+    }
+    return this.number - student.number;
+  }
+
+}
+
+public class Object_sort_lineup {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int student_number = scanner.nextInt();
+
+    Student students[] = new Student[student_number];
+
+    for(int i=0; i<student_number; i++){
+      int student_height = scanner.nextInt();
+      int student_weight = scanner.nextInt();
+
+      students[i] = new Student(i+1, student_height, student_weight);
+    }
+    Arrays.sort(students);
+
+    for(int i=0; i<student_number; i++){
+      System.out.println(students[i].height + " " + students[i].weight + " " + students[i].number);
+    }
+  }
+}
