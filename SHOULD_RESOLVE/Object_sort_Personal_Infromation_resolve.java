3acb1c48af6bf7fcb26e5abf00b9bbf27b405916
notenoughnowjy <jyp4188@gmail.com>
2023-09-01 13:38:50 +0900

    [2023.09.01 ] / 객체 정렬 / 개인정보

---
+++ SHOULD_RESOLVE/Object_sort_Personal_Infromation_resolve.java
@@ -0,0 +1,63 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+class Person{
+    String name;
+    int height;
+    double weight;
+
+    public Person(String name, int height, double weight) {
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+    }
+}
+public class Object_sort_Personal_Infromation_resolve{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int person_number = 5;
+        Person people[] = new Person[person_number];
+
+        for(int i=0; i<person_number; i++){
+            String person_name = scanner.next();
+            int height = scanner.nextInt();
+            double weight = scanner.nextDouble();
+            
+            people[i] = new Person(person_name, height, weight);
+        }
+
+        Arrays.sort(people, new Comparator<Person>() {
+
+            @Override
+            public int compare(Person person1, Person person2) {
+                return person1.name.compareTo(person2.name);
+            }
+        });
+
+        System.out.println("name");
+        for(int i=0; i<person_number; i++){
+            System.out.print(people[i].name + " ");
+            System.out.print(people[i].height + " ");
+            System.out.print(people[i].weight);
+            System.out.println();
+        }
+        System.out.println();
+
+        Arrays.sort(people, new Comparator<Person>() {
+
+            @Override
+            public int compare(Person person1, Person person2) {
+                return person2.height - person1.height;
+            } 
+        });
+
+        System.out.println("height");
+        for (int i = 0; i<person_number; i++){
+            System.out.print(people[i].name + " ");
+            System.out.print(people[i].height + " ");
+            System.out.print(people[i].weight);
+            System.out.println();
+        }
+    }
+}
