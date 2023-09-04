78521bc666be0f82e27d55f5afa68d2114c944eb
notenoughnowjy <jyp4188@gmail.com>
2023-09-04 15:19:01 +0900

    [2023.09.04] / 객체 정렬 / 정렬된 숫자 위치 알아내기 / Should Resolve

---
+++ SHOULD_RESOLVE/Object_sort_sorted_number_position_should_resolve.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Number implements Comparable<Number>{
+  int intnumber, sequence;
+
+  public Number(int intnumber, int sequence) {
+    this.intnumber = intnumber;
+    this.sequence = sequence;
+  }
+
+  @Override
+  public int compareTo(Number number) {
+    if(this.intnumber != number.intnumber){
+      return this.intnumber - number.intnumber;
+    }
+    return this.sequence - number.sequence;
+  }
+}
+public class Object_sort_sorted_number_position {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n = scanner.nextInt();
+    Number number[] = new Number[n];
+    int answer[] = new int[n];
+
+    for(int i=0; i<n; i++){
+      number[i] = new Number(scanner.nextInt(), i);
+    }
+    Arrays.sort(number);
+
+    for(int i=0; i<n; i++){
+      answer[number[i].sequence] = i+1;
+    }
+
+    for(int i=0; i<n; i++){
+      System.out.print(answer[i] + " ");
+    }
+  }
+}
