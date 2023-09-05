1b0c97a3a6446ebe03e8fc750f4424dfad56a1ce
notenoughnowjy <jyp4188@gmail.com>
2023-09-05 13:10:05 +0900

    [2023.09.05] / 날짜와 시간 게산 / Titme to Time

--- 2023.09.05/Date_Time_Calculatio.java
+++ 2023.09.05/Date_Time_Calculatio.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Date_Time_Calculatio {
   public static void main(String[] args) {
-    
+    Scanner scanner = new Scanner(System.in);
+    int hour1 = scanner.nextInt();
+    int minute1 = scanner.nextInt();
+    int hour2 = scanner.nextInt();
+    int minute2 = scanner.nextInt();
+
+    int time1 = (hour1 * 60) + minute1;
+    int time2 = (hour2 * 60) + minute2;
+    System.out.println(time2 - time1);
   }
 }
