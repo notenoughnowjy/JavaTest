118ea362ee84964cf0b4fbffd78605a9588ade53
notenoughnowjy <jyp4188@gmail.com>
2023-09-06 20:13:02 +0900

    [2023.09.06] / 날짜와 시간 계산 / 요일 맞추기

--- 2023.09.06/Date_Time_Calculation_week.java
+++ 2023.09.06/Date_Time_Calculation_week.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Date_Time_Calculation_week{
   public static int[] num_of_days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-  public static String[] week = new String[]{"Sun", "Mon", "Tue", "Wed", "Fri", "Sat"}; // length = 7
+  public static String[] week = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"}; // length = 7
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     int m1 = scanner.nextInt();
@@ -21,9 +21,19 @@ public class Date_Time_Calculation_week{
     // System.out.println(day_diff_2);
     
     String result_week = whatisweek(day_diff_1, day_diff_2);
+
+    System.out.println(result_week);
   }
   private static String whatisweek(int day_diff_1, int day_diff_2) {
-    return null;
+    int week_number = day_diff_2 - day_diff_1;
+
+    while(week_number<0){
+      week_number += 7;
+    }
+
+    String week_return = week[week_number%7];
+
+    return week_return;
   }
   
   private static int calculateDaydiff(int month, int day) {
