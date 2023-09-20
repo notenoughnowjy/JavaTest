125477137f8ee54c7681350c7b0140e332048760
ParkJunYeong <jyp4188@gmail.com>
2023-09-20 17:08:49 +0900

    [2023.09.20] / 구간 칠하기 / 흰검 칠하기

--- 2023.09.20/paint_section_white_black.java
+++ 2023.09.20/paint_section_white_black.java
@@ -4,12 +4,41 @@
 import java.util.Scanner;
 
 public class paint_section_white_black {
+  public static final int Max_K = 100000;
+  public static int[] a = new int[2 * Max_K + 1];
+  public static int[] cntB = new int[2 * Max_K + 1];
+  public static int[] cntW = new int[2 * Max_K + 1];
+  public static int b, w, g;
+
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     int n = scanner.nextInt();
 
-    for(int i=0; i<n; i++){
-      
+    int cur = Max_K;
+    for(int i=1; i<=n; i++){
+      int x = scanner.nextInt();
+      char c = scanner.next().charAt(0);
+      if(c == 'L'){
+        while(x-- > 0){
+          a[cur] = 1;
+          cntW[cur]++;
+          if(x > 0) cur--;
+        }
+      }
+      else{
+        while(x-- > 0){
+          a[cur] = 2;
+          cntB[cur]++;
+          if(x > 0) cur++;
+        }
+      }
     }
+    for(int i=0; i<=2*Max_K; i++){
+      if(cntB[i] >= 2 && cntW[i] >= 2) g++;
+      else if(a[i] == 1) w++;
+      else if(a[i] == 2) b++;
+    }
+    
+    System.out.print(w + " " + b + " " + g);
   }
 }
