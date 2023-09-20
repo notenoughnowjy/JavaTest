41047ecbd1ff08c6c42fbf28b9e8474333b9b174
ParkJunYeong <jyp4188@gmail.com>
2023-09-20 14:23:48 +0900

    [2023.09.20] / 구간 칠하기 / 흰검 칠하기 / Should resolve

---
+++ 2023.09.20/paint_section_white_black.java
@@ -0,0 +1,15 @@
+// https://www.codetree.ai/missions/5/problems/painting-white-black/description
+// 구간 칠기 / 흰검 칠하기
+
+import java.util.Scanner;
+
+public class paint_section_white_black {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n = scanner.nextInt();
+
+    for(int i=0; i<n; i++){
+      
+    }
+  }
+}
