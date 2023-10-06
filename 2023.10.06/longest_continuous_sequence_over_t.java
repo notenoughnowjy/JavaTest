92cc6ba6c30508f9168403b66c341675945843f8
ParkJunYeong <jyp4188@gmail.com>
2023-10-06 14:01:43 +0900

    [2023.10.06] / 최장 연속 부분 수열 / T를 초과하는 연속 부분 수열

--- 2023.10.06/longest_continuous_sequence_over_t.java
+++ 2023.10.06/longest_continuous_sequence_over_t.java
@@ -1,29 +1,29 @@
-// https://www.codetree.ai/missions/5/problems/subsequence-above-t/description
-// 최장 연속 부분 수열 / T를 초과하는 연속 부분 수열
+  // https://www.codetree.ai/missions/5/problems/subsequence-above-t/description
+  // 최장 연속 부분 수열 / T를 초과하는 연속 부분 수열
 
-import java.util.Scanner;
+  import java.util.Scanner;
 
-public class longest_continuous_sequence_over_t{
-  public static int count, result;
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    int n = scanner.nextInt(); // 몇개의 수?
-    int t = scanner.nextInt(); // 기준 정수
+  public class longest_continuous_sequence_over_t{
+    public static int count, result;
+    public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      int n = scanner.nextInt(); // 몇개의 수?
+      int t = scanner.nextInt(); // 기준 정수
 
-    int sequence[] = new int[n];
-    for(int i=0; i<n; i++){
-      sequence[i] = scanner.nextInt();
-    }
-
-    for(int i=0; i<n; i++){
-      if(i >= 1 && sequence[i] > t){
-        count++;
+      int sequence[] = new int[n];
+      for(int i=0; i<n; i++){
+        sequence[i] = scanner.nextInt();
       }
-      else{
-        count = 0;
+
+      for(int i=0; i<n; i++){
+        if(i >= 1 && sequence[i] > t){
+          count++;
+        }
+        else{
+          count = 0;
+        }
+        result = Math.max(result, count);
       }
-      result = Math.max(result, count);
+      System.out.println(result);
     }
-    System.out.println(result);
   }
-}
