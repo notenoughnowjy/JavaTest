8b0bcfd02e89efa675007a0e91e6796e323dff6b
notenoughnowjy <jyp4188@gmail.com>
2024-03-09 11:01:19 +0900

    [2024.03.09] 구간 단위로 완전탐색 / G or H 3

--- 2024/2024_03_08/G_or_H3.java
+++ 2024/2024_03_08/G_or_H3.java
@@ -5,7 +5,7 @@ public class G_or_H3 {
     public static final int max_k = 10000;
 
     public static int[] position_array = new int[max_n];
-    public static int[] real_position_array = new int[max_n];
+    public static char[] real_position_array = new char[max_k+1];
     public static char[] score_char = new char[max_n];
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -21,5 +21,18 @@ public class G_or_H3 {
         for(int i=0; i<n; i++){
             real_position_array[position_array[i]] = score_char[i];
         }
+
+        int result = 0;
+        for(int i=0; i<=max_k-k; i++){
+            int sum_char = 0;
+            for(int j=i; j<=i+k; j++){
+                if(real_position_array[j] == 'G')
+                    sum_char += 1;
+                if(real_position_array[j] == 'H')
+                    sum_char += 2;
+            }
+            result = Math.max(result, sum_char);
+        }
+        System.out.println(result);
     }
 }
