3e66294784004be346a37fd0c08c0c712cebeada
notenoughnowjy <jyp4188@gmail.com>
2024-03-14 16:32:25 +0900

    [2024.03.14] JAVA / BufferedReader 사용해보기

--- 2024/2024_03_14/Char_and_String.java
+++ 2024/2024_03_14/Char_and_String.java
@@ -1,16 +1,37 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.nio.Buffer;
+import java.util.StringTokenizer;
 
 public class Char_and_String {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
-        int i = Integer.parseInt(br.readLine());
 
-        int tepm = Integer.parseInt(s);
+        StringTokenizer st = new StringTokenizer(s);
 
-        System.out.println(s + i + tepm);
+        int[] a = new int[100];
+
+        for(int i=0; i<3; i++)
+            a[i] = Integer.parseInt(st.nextToken());
+
+        String array[] = s.split(" ");
+
+        for(int i=0; i<3; i++){
+//            System.out.println(a[i]);
+        }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        char[] c = new char[100];
+        String[] strings = new String[100];
+        for(int i=0; i<3; i++){
+            strings[i] = Integer.toString(a[i]);
+        }
+        for(int i=0; i<3; i++){
+            bw.write(strings[i]);
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
     }
 }
