44a34c33540537988708c4da42fc0a781749ba59
ParkJunYeong <jyp4188@gmail.com>
2024-03-18 16:49:43 +0900

    [2024.03.18] 백준 - 단어 길이 재기

---
+++ 2024/2024_03_18/Char_length.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Char_length {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String s = br.readLine();
+
+        String result = Integer.toString(s.length());
+        bw.write(result);
+        bw.flush();
+    }
+}
