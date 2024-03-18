069f5bd3ddce50e3087bd070ad03383b125b4ebc
ParkJunYeong <jyp4188@gmail.com>
2024-03-18 17:18:23 +0900

    [2024.03.18] 백준 - 문자 -> 아스키코드

---
+++ 2024/2024_03_18/ASCII_Code.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class ASCII_Code{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        char c = br.readLine().charAt(0);
+        bw.write(Integer.toString((int)c));
+        bw.flush();
+    }
+}
